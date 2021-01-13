c4e1efd783529399d90d371773384725819ce960
AlessandroZ <zanni.alessandro@gmail.com>
2021-01-13 17:33:27 +0100

    enter user pwd interactively

--- Linux/beroot.py
+++ Linux/beroot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import argparse
+import getpass
 import time
 
 from beroot.run import run
@@ -20,12 +21,21 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='%s\nFind a way to BeRoot' % banner, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('--version', action='version', version='%(prog)s 1.1')
     parser.add_argument('--password', dest='password', help='if no NOPASSWD in sudoers, sudo -ll needs user password')
+    parser.add_argument('-p', dest='pwd', action='store_true', default=False, help='enter password interactively')
 
     arguments = parser.parse_args()
     start_time = time.time()
 
     print(banner)
-    run(arguments.password)
+
+    # Manage password
+    password = None
+    if arguments.pwd:
+        password = getpass.getpass('Password:')
+    else: 
+        password = arguments.password
+
+    run(password)
 
     elapsed_time = time.time() - start_time
     print('\nElapsed time = %s' % elapsed_time)
